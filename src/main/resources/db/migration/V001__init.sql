54ca6e7a1658a4f7ebf2b7937ab5468377c41804
Felipe Marinho <felipmarinho@gmail.com>
2020-07-23 05:07:41 -0300

    Adicionando migrações e configuração

---
+++ src/main/resources/db/migration/V001__init.sql
@@ -0,0 +1,39 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  profile varchar(25) NOT NULL
+);
+
+CREATE TABLE collaborators (
+  id SERIAL PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  local varchar(255),
+  team varchar(25) NOT NULL,
+  role varchar(25) NOT NULL,
+  updated_at timestamp NOT NULL,
+  created_at timestamp NOT NULL
+);
+
+CREATE TABLE contacts (
+  id SERIAL PRIMARY KEY,
+  value varchar(255) NOT NULL,
+  type varchar(25) NOT NULL,
+  collaborator_id int NOT NULL
+);
+
+CREATE TABLE professional_experiences (
+  id SERIAL PRIMARY KEY,
+  description varchar(255) NOT NULL,
+  skills varchar(255),
+  collaborator_id int NOT NULL,
+  updated_at timestamp NOT NULL,
+  created_at timestamp NOT NULL
+);
+
+ALTER TABLE contacts
+ADD FOREIGN KEY (collaborator_id) REFERENCES collaborators(id);
+
+ALTER TABLE professional_experiences
+ADD FOREIGN KEY (collaborator_id) REFERENCES collaborators(id);
