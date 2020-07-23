13bc1932be0334de184ddc5075cfffc7b0a24129
Felipe Marinho <felipmarinho@gmail.com>
2020-07-23 05:20:22 -0300

    Adicionando os utils

---
+++ src/main/java/dev/felipemarinho/collaborator/api/utils/PasswordUtils.java
@@ -0,0 +1,28 @@
+package dev.felipemarinho.collaborator.api.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordUtils {
+	
+	private static final Logger log = LoggerFactory.getLogger(PasswordUtils.class);
+	
+	public PasswordUtils() {}
+
+	/**
+	 * Gera um hash utilizando o BCrypt.
+	 * 
+	 * @param senha
+	 * @return String
+	 */
+	public static String buildBcrypt(String password) {
+		if (password == null) {
+			return password;
+		}
+		
+		log.info("Building hash with Bcrypt");
+		BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+		return bCryptEncoder.encode(password);
+	}
+}
