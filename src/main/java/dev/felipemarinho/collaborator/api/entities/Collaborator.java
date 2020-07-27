22cd8d3ad8009ed2fec2719f46fd77f29e583b6e
Felipe Marinho <felipmarinho@gmail.com>
2020-07-27 07:26:49 -0300

    Refatorando anotações

--- src/main/java/dev/felipemarinho/collaborator/api/entities/Collaborator.java
+++ src/main/java/dev/felipemarinho/collaborator/api/entities/Collaborator.java
@@ -4,10 +4,12 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -20,108 +22,46 @@ import dev.felipemarinho.collaborator.api.enums.RoleEnum;
 import dev.felipemarinho.collaborator.api.enums.TeamEnum;
 
 @Entity
-@Table(name = "collaborator")
+@Table(name = "collaborators")
 public class Collaborator implements Serializable{
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 7370894886716753295L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(name = "name", nullable = false)
 	private String name;
+	@Enumerated(EnumType.STRING)
+	@Column(name = "team", nullable = false)
 	private TeamEnum team;
+	@Enumerated(EnumType.STRING)
+	@Column(name = "role", nullable = false)
 	private RoleEnum role;
-	private List<ProfessionalExperiences> experiences;
+
+	@OneToMany(
+		mappedBy = "collaborator",
+		cascade = CascadeType.ALL,
+		fetch = FetchType.EAGER
+	)
+	private List<ProfessionalExperiences> professionalExperiences;
+	@OneToMany(
+		mappedBy = "collaborator",
+		cascade = CascadeType.ALL,
+		fetch = FetchType.EAGER
+	)
 	private List<Contact> contacts;
+	@Column(name = "local", nullable = true)
 	private String local;
+	@Column(name = "created_at", nullable = false)
 	private Date createdAt;
+	@Column(name = "updated_at", nullable = false)
 	private Date updatedAt;
 	
 	public Collaborator() {}
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	@Column(name = "name", nullable = false)
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	@Enumerated(EnumType.STRING)
-	@Column(name = "team", nullable = false)
-	public TeamEnum getTeam() {
-		return team;
-	}
-
-	public void setTeam(TeamEnum team) {
-		this.team = team;
-	}
-
-	@Enumerated(EnumType.STRING)
-	@Column(name = "role", nullable = false)
-	public RoleEnum getRole() {
-		return role;
-	}
-
-	public void setRole(RoleEnum role) {
-		this.role = role;
-	}
-
-	@OneToMany(mappedBy = "collaborator")
-	public List<ProfessionalExperiences> getExperiences() {
-		return experiences;
-	}
-
-	public void setExperiences(List<ProfessionalExperiences> experiences) {
-		this.experiences = experiences;
-	}
-
-	@OneToMany(mappedBy = "collaborator")
-	public List<Contact> getContacts() {
-		return contacts;
-	}
-
-	public void setContacts(List<Contact> contacts) {
-		this.contacts = contacts;
-	}
-	@Column(name = "created_at", nullable = false)
-	public Date getCreatedAt() {
-		return createdAt;
-	}
-	
-	public void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
-	}
-	
-	@Column(name = "updated_at", nullable = false)
-	public Date getUpdatedAt() {
-		return updatedAt;
-	}
-	
-	public void setUpdatedAt(Date updatedAt) {
-		this.updatedAt = updatedAt;
-	}
-
-	@Column(name = "local", nullable = true)
-	public String getLocal() {
-		return local;
-	}
-	
-	public void setLocal(String local) {
-		this.local = local;
-	}
-	
 	@PrePersist
     public void prePersist() {
         final Date now = new Date();
@@ -135,10 +75,82 @@ public class Collaborator implements Serializable{
 		updatedAt = now;
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public TeamEnum getTeam() {
+		return team;
+	}
+
+	public void setTeam(TeamEnum team) {
+		this.team = team;
+	}
+
+	public RoleEnum getRole() {
+		return role;
+	}
+
+	public void setRole(RoleEnum role) {
+		this.role = role;
+	}
+
+	public List<ProfessionalExperiences> getProfessionalExperiences() {
+		return professionalExperiences;
+	}
+
+	public void setProfessionalExperiences(List<ProfessionalExperiences> professionalExperiences) {
+		this.professionalExperiences = professionalExperiences;
+	}
+
+	public List<Contact> getContacts() {
+		return contacts;
+	}
+
+	public void setContacts(List<Contact> contacts) {
+		this.contacts = contacts;
+	}
+
+	public String getLocal() {
+		return local;
+	}
+
+	public void setLocal(String local) {
+		this.local = local;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
 	@Override
 	public String toString() {
 		return "Collaborator [id=" + id + ", name=" + name + ", team=" + team + ", role=" + role + ", experiences="
-				+ experiences + ", contact=" + contacts + ", local=" + local + ", createdAt=" + createdAt
+				+ professionalExperiences + ", contact=" + contacts + ", local=" + local + ", createdAt=" + createdAt
 				+ ", updatedAt=" + updatedAt + "]";
 	}
 
