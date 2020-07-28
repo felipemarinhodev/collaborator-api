160840b5293868004a07dc7c4fe96df88a5b1686
Felipe Marinho <felipmarinho@gmail.com>
2020-07-27 22:04:00 -0300

    inserindo colaborador e xp

---
+++ src/main/resources/db/migration/V003__insert_colaborador_e_xp.sql
@@ -0,0 +1,113 @@
+INSERT INTO collaborators (id,
+  name,
+  local,
+  team,
+  role,
+  updated_at,
+  created_at)
+  VALUES
+  (1, 'Alberto Braga', '', 'CHAPTER_FRONT_END', 'DESENVOLVEDOR', now(), now());
+  
+INSERT INTO professional_experiences (
+  description,
+  skills,
+  collaborator_id,
+  updated_at,
+  created_at
+    ) VALUES (
+    'Empresa 1', 'React', 1, now(), now());
+  
+  INSERT INTO collaborators (id,
+  name,
+  local,
+  team,
+  role,
+  updated_at,
+  created_at)
+  VALUES
+  (2, 'Bryan Adams', '', 'CHAPTER_BACK_END', 'DESENVOLVEDOR', now(), now());
+  
+  INSERT INTO professional_experiences (
+  description,
+  skills,
+  collaborator_id,
+  updated_at,
+  created_at
+    ) VALUES (
+    'Empresa 2', 'React', 2, now(), now());
+  
+  INSERT INTO collaborators (id,
+  name,
+  local,
+  team,
+  role,
+  updated_at,
+  created_at)
+  VALUES
+  (3, 'Carlos Stevão', '', 'CHAPTER_ARQUITETURA', 'ARQUITETURA_SOLUCAO', now(), now());
+  
+  INSERT INTO professional_experiences (
+  description,
+  skills,
+  collaborator_id,
+  updated_at,
+  created_at
+    ) VALUES (
+    'Empresa 3', 'React', 3, now(), now());
+  
+    INSERT INTO collaborators (id,
+  name,
+  local,
+  team,
+  role,
+  updated_at,
+  created_at)
+  VALUES
+  (4, 'Daniel Briand', '', 'CHAPTER_FRONT_END', 'DESENVOLVEDOR', now(), now());
+  
+  INSERT INTO professional_experiences (
+  description,
+  skills,
+  collaborator_id,
+  updated_at,
+  created_at
+    ) VALUES (
+    'Empresa 4', 'React', 4, now(), now());
+  
+  INSERT INTO collaborators (id,
+  name,
+  local,
+  team,
+  role,
+  updated_at,
+  created_at)
+  VALUES
+  (5, 'José Feijão', '', 'SQUAD_ORCAMENTO', 'ANALISTA_SENIOR', now(), now());
+  
+  INSERT INTO professional_experiences (
+  description,
+  skills,
+  collaborator_id,
+  updated_at,
+  created_at
+    ) VALUES (
+    'Empresa 5', 'React', 5, now(), now());
+
+  INSERT INTO collaborators (id,
+  name,
+  local,
+  team,
+  role,
+  updated_at,
+  created_at)
+  VALUES
+  (6, 'Ryan Jones', '', 'SQUAD_ORCAMENTO', 'ARQUITETURA_SOLUCAO', now(), now());
+  
+  INSERT INTO professional_experiences (
+  description,
+  skills,
+  collaborator_id,
+  updated_at,
+  created_at
+    ) VALUES (
+    'Empresa 6', 'React', 6, now(), now());
